45c62c0653766e0f6fab019a2398a0d675b5aaa6
Attila M. Magyar <attila.m.magyar@gmail.com>
2017-12-05 00:29:33 +0100

    minor tweaks in art_track.sh

--- etc/art_track/art_track.sh
+++ etc/art_track/art_track.sh
@@ -8,14 +8,14 @@ ffmpeg \
     -i grid.png \
     -filter_complex \
     "[2:v]crop=1880:300:20:760[grid]; \
-     [0:a]showfreqs=mode=bar:ascale=log:fscale=log:colors=gray gray:s=40x300[sf]; \
-     [sf]crop=40:300:0:0[sfc]; \
-     [sfc]minterpolate=300,tblend=all_mode=average,framestep=5[sfci]; \
-     [sfci]scale=1880x300:flags=neighbor[sfcis]; \
-     [sfcis][grid]overlay[sfcisg]; \
-     [0:a]showwaves=s=940x200:rate=60:colors=white:mode=p2p[sw]; \
-     [sw]scale=1880x400:flags=bicubic[sws]; \
-     [1:v][sfcisg]overlay=x=20:y=760[tmp]; \
+     [0:a]showfreqs=mode=bar:win_size=w2048:ascale=log:fscale=log:colors=0x808080@1.0 0x808080@1.0:s=42x300[sfb]; \
+     [sfb]crop=40:300:0:0[sfbc]; \
+     [sfbc]minterpolate=180,tblend=all_mode=average,framestep=3[sfbci]; \
+     [sfbci]scale=1880x300:flags=neighbor[sfbcis]; \
+     [sfbcis][grid]overlay[sfg]; \
+     [0:a]showwaves=s=1880x134:rate=60:split_channels=0:colors=0xffffff@1.0 0xffffff@1.0:mode=p2p[sw]; \
+     [sw]scale=1880x402:flags=fast_bilinear[sws]; \
+     [1:v][sfg]overlay=x=20:y=760[tmp]; \
      [tmp][sws]overlay=shortest=1:x=20:y=480:format=yuv420[tmp2]; \
      [tmp2]fade=in:0:36[out]" \
     -map "[out]" \
